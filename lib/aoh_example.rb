1b0fa04ba43173f42f31e21647a130c15f5575cf
scruffy-node-9597 <gordonbrian16@gmail.com>
2020-06-10 00:12:59 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -38,7 +38,7 @@ end
 def aoh_update(aoh, row, key, new_value)
   aoh[row] = [new_value]
   aoh[key] = [new_value]
-  aoh_update
+  puts aoh_update
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
